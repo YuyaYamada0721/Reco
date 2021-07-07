18f8b9968b10981ae6f80541356952ab28610916
yuya yamada <yuya19920721@gmail.com>
2021-07-07 16:15:41 +0900

    追加：knowledge scaffold

---
+++ test/system/knowledges_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class KnowledgesTest < ApplicationSystemTestCase
+  setup do
+    @knowledge = knowledges(:one)
+  end
+
+  test "visiting the index" do
+    visit knowledges_url
+    assert_selector "h1", text: "Knowledges"
+  end
+
+  test "creating a Knowledge" do
+    visit knowledges_url
+    click_on "New Knowledge"
+
+    fill_in "Name", with: @knowledge.name
+    click_on "Create Knowledge"
+
+    assert_text "Knowledge was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Knowledge" do
+    visit knowledges_url
+    click_on "Edit", match: :first
+
+    fill_in "Name", with: @knowledge.name
+    click_on "Update Knowledge"
+
+    assert_text "Knowledge was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Knowledge" do
+    visit knowledges_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Knowledge was successfully destroyed"
+  end
+end
