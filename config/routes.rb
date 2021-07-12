4f0f5600464e4bea07924d9308f1b3d8b574bac8
yuya yamada <yuya19920721@gmail.com>
2021-07-12 12:47:04 +0900

    追加：マイページボタンでログインしているユーザの詳細画面へ遷移する設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: %i[show edit update]
   resources :knowledges
   root 'knowledges#index'
   if Rails.env.development?
